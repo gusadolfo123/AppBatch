719114b2e35f471979a54cc7248409d38432cf65
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-19 22:40:01 -0500

    Ajustes

--- src/app.js
+++ src/app.js
@@ -1,37 +1,58 @@
-const {
-    app,
-    BrowserWindow,
-    Menu
-} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron');
 const url = require('url');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'Production') {
-    require('electron-reload')(__dirname, {
-        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
-    });
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
+  });
 }
 
 let MainWindow;
 
-const templateMenu = [{
+const templateMenu = [
+  {
     label: 'File',
-    submenu: [{
+    submenu: [
+      {
         label: 'Add Server',
         accelerator: 'Ctrl+A',
-        click() {
+        click() {},
+      },
+    ],
+  },
+];
 
-        }
-    }]
-}];
+app.on('ready', function() {
+  MainWindow = new BrowserWindow({});
+  MainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'views/index.html'),
+      protocol: 'file',
+      slashes: true,
+    }),
+  );
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
 
-app.on('ready', function () {
-    MainWindow = new BrowserWindow({});
-    MainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }));
-    const mainMenu = Menu.buildFromTemplate(templateMenu);
-    Menu.setApplicationMenu(mainMenu);
+  MainWindow.on('closed', () => {
+    app.quit();
+  });
 });
+
+if (process.env.NODE_ENV !== 'Production') {
+  templateMenu.push({
+    label: 'DevTools',
+    submenu: [
+      {
+        label: 'Show/Hide Dev Tools',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: 'Reload',
+      },
+    ],
+  });
+}
